57a4ba575ebf66f80f48e45b873b541e058d034b
AnkitaSingh3006 <anki3404@gmail.com>
2022-02-27 03:56:56 -0800

    updated code

--- Amazon_replica/form.js
+++ Amazon_replica/form.js
@@ -1,3 +1,15 @@
+//Hamburger for lowerpart of header
+
+const openNav = () => {
+    document.getElementById('mySidenav').style.width ="280px";
+  }
+
+const closeNav = () => {
+    document.getElementById('mySidenav').style.width ="0";
+  }
+
+ //form section
+
 var fullname = document.getElementById("fullname");
 
 function validateForm(){
